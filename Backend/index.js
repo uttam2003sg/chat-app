e8c6ba1d56ce54bd539c9ec39e0129998ce0aa68
uttam2003sg <uttamgupta2003sg@gmail.com>
2024-07-18 20:09:18 +0530

    app-completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+import { dir } from "console";
 
 dotenv.config();
 
@@ -14,6 +15,21 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+
+//--------code for deployment--------
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve();
+
+  app.use(express.static("./Frontend/dist"));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirPath,"./Frontend/dist/","index.html"));
+  })
+}
 
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
@@ -25,9 +41,6 @@ try {
     console.log(error);
 }
 
-//routes
-app.use("/api/user", userRoute);
-app.use("/api/message", messageRoute);
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
