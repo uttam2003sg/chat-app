4b56f44065d1986b02516cb6aa4f6643408534a8
uttam2003sg <uttamgupta2003sg@gmail.com>
2024-07-19 00:35:55 +0530

    url updated again

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chat-app-final-fa8j.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
