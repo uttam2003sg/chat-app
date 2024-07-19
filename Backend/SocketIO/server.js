02718ce17839d7beedbc8ca7644aa3777de72208
uttam2003sg <uttamgupta2003sg@gmail.com>
2024-07-19 12:54:53 +0530

    url_updated

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-app-i7la.onrender.com/login"],
+    origin: ["http://localhost:4002/"],
     methods: ["GET", "POST"],
   },
 });
